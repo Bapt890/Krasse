5ed7ddf0622a6867c9a2235adcd1e418da864241
Bapt890 <bapt890@gmail.com>
2025-05-10 22:25:20 +0200

    Bar de progression de la crasse

--- scn/tile_map_layer_slime.gd
+++ scn/tile_map_layer_slime.gd
@@ -1,6 +1,7 @@
 extends TileMapLayer
 
 @export var units : Array[Unit]
+@export var bar : TextureRect
 
 func _process(_delta):
 	for unit : Unit in units:
@@ -9,3 +10,14 @@ func _process(_delta):
 			and get_cell_tile_data(tile).get_custom_data("team") > -1:
 			unit.hurt(1)
 		set_cell(tile, 1, Vector2(unit.team + 1, 0))
+	
+	var purple : float = 0
+	var green : float = 0
+	for cell in get_used_cells():
+		match get_cell_tile_data(cell).get_custom_data("team"):
+			0:
+				purple += 1
+			1:
+				green += 1
+	bar.material.set_shader_parameter("purple_team", purple/get_used_cells().size())
+	bar.material.set_shader_parameter("green_team", 1 - green/get_used_cells().size())
