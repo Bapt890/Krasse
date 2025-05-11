c7cba64bf79636c1777615a93ad6c38af0dc0dfe
AKaulitz <ibaney.axel84@gmail.com>
2025-05-11 11:47:22 +0200

    HUD connecté

--- ResourceManager.gd
+++ ResourceManager.gd
@@ -1,6 +1,11 @@
 extends Node
 
+signal resources_updated
+
 @export var resources : Array[int] = [100, 0]
+
+func get_resource(type : int):
+	return resources[type]
 
 func addResource(type : int, amount : int = 1):
 	resources[type] += amount
@@ -10,6 +15,6 @@ func substractResource(type : int, amount : int = 1):
 	resources[type] -= amount
 	updateUI()
 
-
 func updateUI():
+	emit_signal("resources_updated")
 	print(resources)
